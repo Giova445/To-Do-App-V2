56134281ff76a8db1d8ccd4084ac83d42fc4948e
Giovanny Moreno <giova1445@hotmail.com>
2023-03-15 21:49:41 -0500

    Refactoring APPUI and deleting todoContext

--- src/App/index.js
+++ src/App/index.js
@@ -1,15 +1,77 @@
 import React from "react";
-import { TodoProvider } from "../TodoContext";
-import { AppUI } from "./AppUI";
-
+import { useTodos } from "./useTodos";
+import { TodoHeader } from "../TodoHeader";
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from "../TodoSearch";
+import { TodoList } from "../TodoList";
+import { TodoItem } from "../TodoItem";
+import { TodoButton } from "../TodoButton";
+import { Modal } from "../Modal";
+import { TodoForm } from "../TodoForm";
+import { TodosError } from "../TodosError";
+import { TodosLoading } from "../TodosLoading";
+import { EmptyTodos } from "../EmptyTodos";
+import { Toggle } from "../TodoDarkMode";
+import {  keepTheme } from "../TodoDarkMode/theme";
+import "./App.css"
 
 
 function App() {
+  const { error,
+          loading,
+          filterTodos,
+          toggleCompleteTodo,
+          deleteTodo,
+          openModal,
+          setOpenModal,
+          totalTodos,
+          completedTodos,
+          searchValue,
+          setSearchValue,
+          addTodo,
+  }=useTodos();
   return (
-    <TodoProvider>
-      <AppUI/>
-    </TodoProvider>
+    <>
+        <TodoHeader>
+            <TodoCounter 
+                totalTodos={totalTodos} 
+                completedTodos={completedTodos}
+            />    
+            <TodoSearch 
+                searchValue={searchValue}
+                setSearchValue={setSearchValue}
+            />
+        </TodoHeader>
+
+        <TodoList>
+            {error && <TodosError error={error}/>}
+            {loading && <TodosLoading />}
+            {(!loading && !filterTodos.length) && <EmptyTodos/>}
+            {filterTodos.map(todo=> (
+            <TodoItem
+                 key={todo.text}
+                text={todo.text}
+                complete={todo.completed}
+                onComplete={() => toggleCompleteTodo(todo.text)}
+                onDelete={() => deleteTodo(todo.text)}
+                 />
+                ))}
+        </TodoList>
+        {!!openModal && (
+            <Modal>
+                < TodoForm 
+                  addTodo={addTodo}
+                  setOpenModal={setOpenModal
+                } />
+                {/* <p>{filterTodos[0]?.text}</p> */}
+            </Modal>
+        )}
+        <TodoButton  setOpenModal={setOpenModal} openModal={openModal}/>
+        
+        <Toggle keepTheme={keepTheme} Toggle={Toggle}></Toggle>
+    </>
   );
+
 }
 
 export default App;
--- src/TodoButton/index.jsx
+++ src/TodoButton/index.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import './TodoButton.css';
-import { TodoContext } from '../TodoContext';
 
-const TodoButton = () => {
-  const { setOpenModal } = React.useContext(TodoContext);
+const TodoButton = ( { setOpenModal } ) => {
   const onClick = () => setOpenModal(prevState => !prevState);
 
   return (
